a2d372b7300748f3ae9c8624fa757acc653e60d5
Scala Steward <me@scala-steward.org>
2021-03-13 13:03:45 +0100

    Update json4s-native to 3.5.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
       "com.typesafe.akka" %% "akka-actor" % "2.5.13",
       "com.typesafe.akka" %% "akka-stream" % "2.5.13",
       "com.typesafe.akka" %% "akka-http" % "10.1.1",
-      "org.json4s" %% "json4s-native" % "3.5.4",
+      "org.json4s" %% "json4s-native" % "3.5.5",
       "ch.qos.logback" % "logback-classic" % "1.1.7",
       "com.typesafe.akka" %% "akka-http-testkit" % "10.1.1" % "test,it",
       "org.scalatest" %% "scalatest" % "3.0.0" % "test,it"

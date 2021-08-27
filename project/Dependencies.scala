ecc9f06470e11d8569f859d68994b6ef20dfb89f
Anand Singh <imanandksingh@gmail.com>
2021-08-27 00:33:13 +0000

    Update akka-actor, akka-stream, ... to 2.6.16

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,12 +4,12 @@ object Dependencies {
 
   lazy val dependencies =
     Seq(
-      "com.typesafe.akka" %% "akka-actor" % "2.6.15",
-      "com.typesafe.akka" %% "akka-stream" % "2.6.15",
+      "com.typesafe.akka" %% "akka-actor" % "2.6.16",
+      "com.typesafe.akka" %% "akka-stream" % "2.6.16",
       "com.typesafe.akka" %% "akka-http" % "10.2.5",
       "org.json4s" %% "json4s-native" % "4.0.1",
       "ch.qos.logback" % "logback-classic" % "1.2.4",
-      "com.typesafe.akka" %% "akka-testkit" % "2.6.15" % "test,it",
+      "com.typesafe.akka" %% "akka-testkit" % "2.6.16" % "test,it",
       "com.typesafe.akka" %% "akka-http-testkit" % "10.2.5" % "test,it",
       "org.scalatest" %% "scalatest" % "3.2.9" % "test,it"
     )

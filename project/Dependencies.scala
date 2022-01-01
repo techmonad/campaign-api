21e7bec3cf1b20dfeb2cf83d38f53cdbb0c58453
Anand Singh <imanandksingh@gmail.com>
2022-01-01 00:21:25 +0000

    Update logback-classic from 1.2.6 to 1.2.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
       "com.typesafe.akka" %% "akka-stream" % "2.6.17",
       "com.typesafe.akka" %% "akka-http" % "10.2.6",
       "org.json4s" %% "json4s-native" % "4.0.3",
-      "ch.qos.logback" % "logback-classic" % "1.2.6",
+      "ch.qos.logback" % "logback-classic" % "1.2.10",
       "com.typesafe.akka" %% "akka-testkit" % "2.6.17" % "test,it",
       "com.typesafe.akka" %% "akka-http-testkit" % "10.2.6" % "test,it",
       "org.scalatest" %% "scalatest" % "3.2.10" % "test,it"

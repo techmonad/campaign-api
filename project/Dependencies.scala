e8ec5fe54fb0fa1d78ab632ef5267265281588cc
Anand Singh <imanandksingh@gmail.com>
2022-01-01 00:21:29 +0000

    Update akka-http, akka-http-testkit from 10.2.6 to 10.2.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,11 +6,11 @@ object Dependencies {
     Seq(
       "com.typesafe.akka" %% "akka-actor" % "2.6.17",
       "com.typesafe.akka" %% "akka-stream" % "2.6.17",
-      "com.typesafe.akka" %% "akka-http" % "10.2.6",
+      "com.typesafe.akka" %% "akka-http" % "10.2.7",
       "org.json4s" %% "json4s-native" % "4.0.3",
       "ch.qos.logback" % "logback-classic" % "1.2.6",
       "com.typesafe.akka" %% "akka-testkit" % "2.6.17" % "test,it",
-      "com.typesafe.akka" %% "akka-http-testkit" % "10.2.6" % "test,it",
+      "com.typesafe.akka" %% "akka-http-testkit" % "10.2.7" % "test,it",
       "org.scalatest" %% "scalatest" % "3.2.10" % "test,it"
     )
 }
